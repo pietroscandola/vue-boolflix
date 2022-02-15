b4617116820836751c2f6b500c735aa44edb02de
pietroscandola <pietro.scandolavr@gmail.com>
2022-02-15 12:59:04 +0100

    add no-poster-available

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="col-3 my-3">
+    <img class="cover" :src="flagSrc" :alt="item.title" />
     <div>{{ item.title || item.name }}</div>
     <div>{{ item.original_title || item.original_name }}</div>
-    <div
-      v-if="item.original_language === 'en' || item.original_language === 'it'"
-    >
+    <div v-if="hasFlag">
       <img
+        class="flag"
         :src="require(`../assets/img/${item.original_language}.png`)"
         :alt="item.original_language"
       />
     </div>
     <div v-else>{{ item.original_language }}</div>
-    <div>{{ item.vote_average }}</div>
+    <div>
+      {{ item.vote_average }}
+      <i class="fa-solid fa-star"></i>
+    </div>
   </div>
 </template>
 
@@ -23,10 +26,32 @@ export default {
     return {
       flags: ["it", "en"],
       selectedFilms: "",
-      selectedTVSeries: "",
+      selectedTVSeries: "",       
     };
+  },
+
+  computed: {
+    hasFlag() {
+      return this.flags.includes(this.item.original_language);
+    },
+
+    flagSrc() {
+      if (this.item.poster_path) {
+        return `https://image.tmdb.org/t/p/w342/${this.item.poster_path}`;
+      } else {
+        return require("../assets/img/no-poster-available.jpg");
+      }
+    },
   },
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.flag {
+  width: 50px;
+}
+
+.cover {
+  width: 90%;
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -24,7 +24,5 @@ export default {
 </script>
 
 <style>
-img {
-  width: 50px;
-}
+
 </style>
